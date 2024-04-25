06ee619816227b1d32a4095f94ea3131b490834c
JoaquinBenitezVelazquez <velazquezjoaquin163@gmail.com>
2024-04-25 20:42:49 -0300

    practica-4

---
+++ practica4/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MostrarTexto from "./components/table/table";
+
+function App() {
+  return (
+    <>
+      <MostrarTexto />
+    </>
+  );
+}
+
+export default App;
---
+++ practica4/src/components/table/table.jsx
@@ -0,0 +1,9 @@
+function MostrarTexto() {
+  return (
+    <>
+      <h1>Hola nazi</h1>
+    </>
+  );
+}
+
+export default MostrarTexto;
